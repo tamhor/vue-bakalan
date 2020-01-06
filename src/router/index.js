723f1348a98c272657c3455f61f1706b99f5c32a
Rohmat Hidayat <tamhor.hidayat@gmail.com>
2020-01-07 01:31:06 +0700

    working profile

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Profile from '../views/Profile/Profile.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/profile',
+    name: 'profil',
+    component: Profile
   },
   {
     path: '/about',
